dca5ba84165adbe048c36a224279b11bd76b2d7d
Chadwick <83472455+productivechad@users.noreply.github.com>
2021-06-21 13:50:59 -0400

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+
+
+function My_First_Function() {          //Defining a function and naming it
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text") .innerHTML = result;
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate") .innerHTML = sentence;
+}
