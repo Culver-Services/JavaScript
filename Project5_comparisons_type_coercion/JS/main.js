18fdf2da9e55f80abe50974b9e4ab7eba77ce90f
Chadwick <83472455+productivechad@users.noreply.github.com>
2021-06-25 12:32:43 -0400

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,16 @@
+function not_function() {
+document.getElementById("Not").innerHTML = ! (20 < 10 && 8 > 9); //The ! (not) operator checks whether or not something is false
+}
+
+function yes_function() {
+    document.getElementById("Yes").innerHTML = (20 < 10 || 2 > 9); //The operator checks whether or not something is true
+    }
+
+function my_function() {    //Regular my_function 
+    document.getElementById("Not").innerHTML = 8;
+A = "5";
+B = "2";
+document.write (A === B);
+document.write(typeof 3)
+document.write(B == A)
+}
